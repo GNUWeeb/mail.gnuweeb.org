a04173a43dee09ae3dea28bd36d5fc665b02f3db
Ammar Faizi <ammarfaizi2@gmail.com>
2021-05-24 23:17:24 +0700

    Update register page
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- public/register.php
+++ public/register.php
@@ -25,53 +25,79 @@ if (isset($_SESSION["user"])) {
 			<table class="reg-table">
 				<tbody>
 					<tr>
-						<td align="center" colspan="4"><div class="hdr-sep-uinfo"><span class="tbl-hdr">Enter User Info</span></div></td>
+						<td align="center" colspan="3">
+							<div class="hdr-sep-uinfo"><span class="tbl-hdr">Enter User Info</span></div>
+						</td>
 					</tr>
 					<tr>
 						<td align="left"><span class="mandat">*&nbsp;</span>First Name</td>
 						<td align="center">:</td>
-						<td align="left" colspan="2"><input type="text" name="first_name" required="1"/></td>
+						<td align="left"><input type="text" name="first_name" required="1"/></td>
 					</tr>
 					<tr>
 						<td align="left">&nbsp;&nbsp;Last Name</td>
 						<td align="center">:</td>
-						<td align="left" colspan="2"><input type="text" name="last_name" required="1"/></td>
+						<td align="left"><input type="text" name="last_name"/></td>
 					</tr>
 					<tr>
 						<td align="left"><span class="mandat">*&nbsp;</span>Gender</td>
 						<td align="center">:</td>
-						<td align="left" colspan="2">
+						<td align="left">
 							<input type="radio" name="gender" value="male" required="1"/>Male
 							<input type="radio" name="gender" value="female" required="1"/>Female
 							<input type="radio" name="gender" value="secret" required="1"/>No answer
 						</td>
 					</tr>
 					<tr>
-						<td align="center" colspan="4"><div class="hdr-sep-cracc"><span class="tbl-hdr">Create an Account</span></div></td>
+						<td align="left">&nbsp;&nbsp;GitHub Username</td>
+						<td align="center">:</td>
+						<td align="left"><input type="text" name="github_username"/></td>
+					</tr>
+					<tr>
+						<td align="center" colspan="3">
+							<div class="hdr-sep-cracc"><span class="tbl-hdr">Create an Account</span></div>
+						</td>
 					</tr>
 					<tr>
 						<td align="left"><span class="mandat">*&nbsp;</span>Email</td>
 						<td align="center">:</td>
-						<td align="center"><input type="text" name="username" required="1" size="10"/></td>
-						<td align="left">@gnuweeb.org</td>
+						<td align="left"><input type="text" name="username" required="1" size="10"/>&nbsp;@gnuweeb.org</td>
 					</tr>
 					<tr>
 						<td align="left"><span class="mandat">*&nbsp;</span>Password</td>
 						<td align="center">:</td>
-						<td align="left" colspan="2"><input type="password" name="password" required="1"/></td>
+						<td align="left"><input type="password" name="password" required="1"/></td>
 					</tr>
 					<tr>
 						<td align="left"><span class="mandat">*&nbsp;</span>Retype Password</td>
 						<td align="center">:</td>
-						<td align="left" colspan="2"><input type="password" name="cpassword" required="1"/></td>
+						<td align="left"><input type="password" name="cpassword" required="1"/></td>
 					</tr>
 				</tbody>
 				<tfoot>
 					<tr>
-						<td colspan="4"><img id="captcha-img"/></td>
+						<td colspan="3">
+							<div class="captcha-area">
+								<div class="captcha-inst">
+									Please solve this captcha problem to make sure you're a human!
+								</div>
+								<div class="captcha-cage">
+									<img id="captcha-img"/>
+								</div>
+								<br/>Captcha Answer:<br/>
+								<input type="text" name="captcha_answer" id="captcha-answer"/>
+							</div>
+						</td>
 					</tr>
 					<tr>
-						<td colspan="4">Already have an account? <a href="login.php?ref=register">Login</a></td>
+						<td colspan="3">
+							<div class="reg-btn-cage">
+								<button type="submit">Register</button>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3">Already have an account? <a href="login.php?ref=register">Login</a></td>
 					</tr>
 				</tfoot>
 			</table>
