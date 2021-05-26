0404bc8a6791be891c1e2bbc695768395858b2b9
Sprtcrnbry <sprtcrnbry@gmail.com>
2021-05-26 21:49:33 +0700

    (trying to be) Responsive, Adaptive Dark Mode, and Hack fonts (monospace)
    Signed-off-by: Sprtcrnbry <sprtcrnbry@gmail.com>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- public/home.php
+++ public/home.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION["user"])) {
 <head>
 	<title>GNUWeeb Mail</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/base.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.00" />
 </head>
 <body>
 	<center>
