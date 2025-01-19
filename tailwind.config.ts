0b279ed175fa6882ea6619a251c5144c8034c8a7
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-01-19 08:11:20 +0700

    feat(fonts): add Google fonts
    Import Google Fonts using CSS `@import` and configure the font families
    in `app.css` and `tailwind.config.ts`.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20250119011136.1254-4-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,19 @@ import containerQueries from "@tailwindcss/container-queries";
 import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "Rubik", "Geist", ...defaultTheme.fontFamily.sans],
+        rubik: ["Rubik", "Inter", "Geist", ...defaultTheme.fontFamily.sans],
+        geist: ["Geist", "Inter", "Rubik", ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
 
   plugins: [typography, forms, containerQueries]
