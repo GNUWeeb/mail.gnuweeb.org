c7af7ded9a525d3970b508ff24fd5f2ddc25c6a9
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-01-19 08:11:27 +0700

    feat: initial hooks, schemas, typings for the login functions
    added some lib for the login functionality:
    - hooks/auth: this hooks include base logic for the auth.
    - hooks/http: this hooks used for http request.
    - schemas/login: this schema is to create a schema for the login form.
    - typings/credential: this types stores user-related type
    - typings/http: this types stores http-related type
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20250119011136.1254-11-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ src/lib/typings/credential.d.ts
@@ -0,0 +1,7 @@
+export interface User {
+  user_id: number;
+  full_name: string;
+  gender: string;
+  username: string;
+  role: string;
+}
--- src/lib/typings/index.ts
+++ src/lib/typings/index.ts
@@ -1,3 +1,13 @@
 import type { RecordString, Navigations, LabelAndValue, MailConfig } from "./common";
+import type { UseHttpProps, LoginResponse } from "./http";
+import type { User } from "./credential";
 
-export type { RecordString, Navigations, LabelAndValue, MailConfig };
+export type {
+  RecordString,
+  Navigations,
+  LabelAndValue,
+  MailConfig,
+  UseHttpProps,
+  LoginResponse,
+  User
+};
