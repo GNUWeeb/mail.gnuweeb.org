3e80341ea1888a35be8429fb8f8196e68c107fd4
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-01-19 08:11:18 +0700

    feat: initial front-end
    initial commit for front-end setup.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20250119011136.1254-2-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: "dist",
+      assets: "dist",
+      precompress: false,
+      strict: true
+    }),
+    alias: {
+      $components: "src/lib/components",
+      $stores: "src/lib/stores",
+      $constants: "src/lib/constants",
+      $utils: "src/lib/utilities",
+      $typings: "src/lib/typings"
+    }
+  }
+};
+
+export default config;
