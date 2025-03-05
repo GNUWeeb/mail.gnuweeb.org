fdcf495a34f38a00d5dd22afcd53d876978ebd78
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-03-05 21:40:10 +0700

    feat(icons): Add social icons
    This social icons will be used in the profile form.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Acked-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20250305144018.267-12-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ src/lib/components/icons/icon-twitter.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+
+  let { href, class: className }: HTMLAttributes<SVGElement> & { href?: string } = $props();
+</script>
+
+{#if href}
+  <a {href} aria-label="Social Icon" target="_blank" rel="noopener noreferrer">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      class={className}
+    >
+      <path
+        fill="currentColor"
+        d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584l-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
+      />
+    </svg>
+  </a>
+{:else}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    class={className}
+  >
+    <path
+      fill="currentColor"
+      d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584l-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
+    />
+  </svg>
+{/if}
