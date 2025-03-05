c9d5e8de2e1029f5bd120c01a42566b73cb85f3f
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-03-05 21:40:11 +0700

    feat(typing/enum): add Gender and IsActive enum
    Added Gender and IsActive enum to make it readable and increase
    developer experience.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20250305144018.267-13-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/lib/typings/index.ts
+++ src/lib/typings/index.ts
@@ -1,8 +1,17 @@
-import type { RecordString, Navigations, LabelAndValue, MailConfig } from "./common";
+import type {
+  RecordString,
+  Navigations,
+  LabelAndValue,
+  MailConfig,
+  Gender,
+  IsActive
+} from "./common";
 import type { ResponseAPI, LoginResponse, RenewTokenResponse } from "./http";
 import type { User } from "./credential";
 
 export type {
+  Gender,
+  IsActive,
   RecordString,
   Navigations,
   LabelAndValue,
