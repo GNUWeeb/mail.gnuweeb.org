a183f0406ea3a436b8435b0ced7b5098748a1d99
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-03-11 21:11:29 +0700

    chore(profile): refetch user data on profile
    This commit will refetch user data to get the latest user data on
    `/settings/profile` page. This method is to prevent users manipulating
    any credentials on the localStorage, with refetching new data, it will
    be replaced every time users navigating or reloading the
    /settings/profile page.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20250311141130.15924-3-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/routes/(protected)/settings/profile/+page.ts
+++ src/routes/(protected)/settings/profile/+page.ts
@@ -3,19 +3,37 @@ import { zod } from "sveltekit-superforms/adapters";
 import { superValidate } from "sveltekit-superforms";
 import { profileSchema } from "$lib/schemas/profile-schema";
 import { useAuth } from "$lib/hooks/auth.svelte";
+import * as typing from "$typings"
+import http from "$lib/hooks/http.svelte";
+import { redirect } from "@sveltejs/kit";
 
 export const load: PageLoad = async () => {
   const auth = useAuth();
-  auth.refresh();
 
-  const data = {
-    username: auth.user?.username,
-    full_name: auth.user?.full_name,
-    ext_email: auth.user?.ext_email,
-    gender: auth.user?.gender,
-    socials: auth.user?.socials
+  const { status, data } = await http<{ user_info: typing.User }>({
+    params: { action: "get_user_info" }
+  });
+
+  if (status !== 200) {
+    localStorage.setItem("gwm_invalid_creds", String(1));
+    auth.clear();
+    return redirect(307, "/");
+  }
+
+  auth.save({
+    token: data.res?.renew_token?.token,
+    token_exp_at: data.res?.renew_token?.token_exp_at,
+    user_info: data.res?.user_info
+  });
+
+  const initialData = {
+    username: data.res?.user_info?.username,
+    full_name: data.res?.user_info?.full_name,
+    ext_email: data.res?.user_info?.ext_email,
+    gender: data.res?.user_info?.gender,
+    socials: data.res?.user_info?.socials
   };
 
-  const form = await superValidate(data, zod(profileSchema));
+  const form = await superValidate(initialData, zod(profileSchema));
   return { form };
 };
